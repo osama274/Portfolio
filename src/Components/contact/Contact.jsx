42c64c7c12aab4e42d01c7d4159043b2fd8ab1aa
Osama <alhaj2904@gmail.com>
2022-03-12 11:02:04 +0100

    nn

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_ohhe5ri', 'template_vj65zdg',  'user-f1Pk0qpUx4klJRbZb')
+    emailjs.sendForm('service_ohhe5ri', 'template_vj65zdg',  'f1Pk0qpUx4klJRbZb')
      e.target.reset()
   };
   return (
